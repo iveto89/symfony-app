c1fa45ab72c9f318695ce28a40d993f884fdf5d8
iva <ivetoo89@gmail.com>
2019-09-17 18:13:59 +0300

    create entity, migration and index page

--- src/Command/HelloCommand.php
+++ src/Command/HelloCommand.php
@@ -6,10 +6,14 @@ use App\Service\Greeting;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class HelloCommand extends Command
 {
+    protected static $defaultName = 'app:say-hello';
+
     /**
      * @var Greeting
      */
@@ -24,9 +28,10 @@ class HelloCommand extends Command
 
     protected function configure()
     {
-        $this->setName('app:say-hello')
-            ->setDefinition('Says hello to the user')
-            ->addArgument('name', InputArgument::REQUIRED);
+        $this
+            ->setDefinition([new InputOption('Says hello to the user')])
+            ->addArgument('name', InputArgument::REQUIRED, 'Who do you want to greet?');
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -37,5 +42,20 @@ class HelloCommand extends Command
             '------------------',
             ''
         ]);
+        $output->write($this->greeting->greet($name));
+
+
+//        $io = new SymfonyStyle($input, $output);
+//        $arg1 = $input->getArgument('arg1');
+//
+//        if ($arg1) {
+//            $io->note(sprintf('You passed an argument: %s', $arg1));
+//        }
+//
+//        if ($input->getOption('option1')) {
+//            // ...
+//        }
+//
+//        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
     }
 }
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,9 +5,18 @@ namespace App\Controller;
 use App\Service\Greeting;
 use App\Service\VeryBadDesign;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
+/**
+ * Class BlogController
+ * @package App\Controller
+ * @Route("/blog")
+ */
 class BlogController extends AbstractController
 {
     /**
@@ -19,15 +28,30 @@ class BlogController extends AbstractController
      * @var VeryBadDesign
      */
     private $badDesign;
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
 
     /**
      * BlogController constructor.
      * @param Greeting $greeting
      */
-    public function __construct(Greeting $greeting, VeryBadDesign $badDesign)
+    public function __construct(
+        Greeting $greeting,
+        VeryBadDesign $badDesign,
+        SessionInterface $session,
+        RouterInterface $router
+    )
     {
         $this->greeting = $greeting;
         $this->badDesign = $badDesign;
+        $this->session = $session;
+        $this->router = $router;
     }
 
     /**
@@ -35,10 +59,44 @@ class BlogController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/", name="blog_index")
      */
-    public function index(Request $request)
+    public function index()
     {
-        return $this->render('base.html.twig', ['message' => $this->greeting->greet(
-            $request->get('name')
-        )]);
+        return $this->render('blog/index.html.twig', [
+            'posts' => $this->session->get('posts')
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="blog_add")
+     */
+    public function add()
+    {
+        $posts = $this->session->get('posts');
+        $posts[uniqid()] = [
+            'title' => 'A random title '.rand(1, 500),
+            'text' => 'Some random text '.rand(1, 500),
+            'date' => new \DateTime()
+        ];
+        $this->session->set('posts', $posts);
+
+        return new RedirectResponse($this->router->generate('blog_index'));
+    }
+
+    /**
+     * @param $id
+     * @Route("/show/{id}", name="blog_show")
+     */
+    public function show($id)
+    {
+        $posts = $this->session->get('posts');
+
+        if (!$posts || !isset($posts[$id])) {
+            throw new NotFoundHttpException('Post not found');
+        }
+
+        return $this->render('blog/post.html.twig', [
+            'id' => $id,
+            'post' => $posts[$id]
+        ]);
     }
 }
